c8582db748b50fa264481a908ba190933185f32a
Davit Kroyan <dkroyan@gmail.com>
2017-11-02 14:27:56 +0400

    FIX:app window height

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
 
 
   // Create the browser window.
-  win = new BrowserWindow({width: 600, height: 800})
+  win = new BrowserWindow({width: 600, height: 600})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
